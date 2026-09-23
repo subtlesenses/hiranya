c9b09825c9c299da1bf6f73f9996b2d6302312b6
subtlesenses <114979456+subtlesenses@users.noreply.github.com>
2026-09-23 08:56:15 +0545

    Embed Beds24 in a styled Hiranya booking page

--- src/lib/booking.ts
+++ src/lib/booking.ts
@@ -1,4 +1,4 @@
-export const bookingUrl = 'https://beds24.com/booking2.php?propid=355842&cur=USD'
+export const bookingUrl = '/book/?cur=USD'
 export const roomIds: Record<string, string> = {
   Mahalaxmi: '733187', Mahakali: '733189', Brahmayani: '733192',
   Indrayani: '733190', Rudrayani: '733191', Vaishnavi: '733429',
