ed02314626277e9f18d9e56fe05060b1020d0995
subtlesenses <114979456+subtlesenses@users.noreply.github.com>
2026-09-26 20:44:25 +0545

    Configure Hiranya Studio deployment hostname (#10)

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -4,4 +4,5 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID,
     dataset: process.env.SANITY_STUDIO_DATASET || 'production',
   },
+  studioHost: 'hiranya-guest-house-s7s7kahm',
 })
